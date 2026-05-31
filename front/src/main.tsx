63f4a5832996d71183316d5017a891844349242d
v.penkov <v.penkov@bars.grou>
2026-05-31 15:15:12 +0300

    fix git Actions

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -10,7 +10,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="app-theme">
-      <BrowserRouter>
+      <BrowserRouter basename="/diplomNew">
         <AuthProvider>
           <AppToaster />
           <AppRouter />
